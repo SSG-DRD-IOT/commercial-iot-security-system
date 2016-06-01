dc3c6f4be1f938a8939c61b541645607ed27d8d5
danbudanov <danbudanov@gmail.com>
2016-06-01 15:41:52 -0700

    Backprojection (somewhat) working
    Am not certain if this is the best way to go about detecting objects

--- opencv/featureDetection/features/features.py
+++ opencv/featureDetection/features/features.py
@@ -1,2 +1,18 @@
 """
 Understanding Features
+
+ex. how to put together a puzzle on a computer?
+-> look for specific patterns/features that are unique and can easily be tracked and compared
+
+(worst) flat area -> edges -> corners (best)
+corners, and in some cases blobs, good features
+
+How to find features / corners?
+    look at regions in images with maximum variation when moved in all regions around it
+    -> Feature Detection
+        once have features and its description, can find same features in all images and
+            align
+            stitch
+            whatever
+        to them.
+"""
