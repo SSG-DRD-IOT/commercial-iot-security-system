2a1bffc6f3ce10a89d21e9e67d0366ea7e30e460
Daniel Holmlund <daniel.w.holmlund@intel.com>
2016-07-14 10:28:53 -0700

    Sends basic MQTT alert

--- opencv/commercial/Face/utils/trigger.py
+++ opencv/commercial/Face/utils/trigger.py
@@ -1,5 +1,20 @@
 import time
+import sys
+import paho.mqtt.publish as mqtt
+
+
+'''
+    {
+        "event": "FaceDetect",
+        "uri"  : "http://gateway/filename.avi",
+        "facenum" : 4
+        "timestamp" : Date.now()
+        "offsetTime" : 
+    }
+
+'''
 
 def trigger(number):
+    mqtt.single("sensors/temperature/data", "I see a face", hostname="localhost")
     print "Event triggered:", number, "detected!"
     return 1
