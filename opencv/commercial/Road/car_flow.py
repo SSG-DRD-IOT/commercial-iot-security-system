65c216764277c1cb76bc2c17a04a564aeb31bfcb
danbudanov <danbudanov@gmail.com>
2016-08-01 15:31:15 -0700

    PSA: other countries have cars, too
    the road markings did not necessarily have to be 10 feet long,
    especially if they were not in the us
    
    added in ft/s -> mph conversion

--- opencv/commercial/Road/car_flow.py
+++ opencv/commercial/Road/car_flow.py
@@ -1,6 +1,17 @@
+###############################################################################
+# Author: Daniil Budanov
+# Contact: danbudanov@gmail.com
+# Summer Internship - 2016
+###############################################################################
+# Title: car_flow.py
+# Project: Car Motion Detection
+# Description:
+#   uses optical flow to find car displacement vectors
+#   then, uses a perspective transform to estimate car velocity
+# Last Modified: 8.1.2016
+###############################################################################
 import cv2
 import numpy as np
-import time
 
 cap = cv2.VideoCapture("carsCrop.avi")
 
@@ -16,36 +27,47 @@ def regionSelect(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print "{}: ( {}, {} )".format(param, x, y)
 
+
+# small macros
+def ftps2mph(ftps):
+    return ftps * 3600 / 5280.
+
 cv2.namedWindow("road")
 cv2.namedWindow("transformed")
 
 cv2.setMouseCallback("road", regionSelect, "road")
 cv2.setMouseCallback("transformed", regionSelect, "rectangle")
 
+# road ROI corners in original frame
 p00 = (210, 204)
 p01 = (423, 210)
 p10 = (82, 355)
 p11 = (544, 356)
 
-l01 = ptAvg(p00, p01)
-l02 = ptAvg(p10, p11)
+# # corners of perspective transform of ROI
+# q00 = (0, 0)
+# q01 = (250, 0)
+# q10 = (0, 500)
+# q11 = (250, 500)
 
+# corners of perspective transform of ROI
 q00 = (0, 0)
-q01 = (250, 0)
-q10 = (0, 500)
-q11 = (250, 500)
-l11 = ptAvg(q00, q01)
-l12 = ptAvg(q10, q11)
+q01 = (350, 0)
+q10 = (0, 700)
+q11 = (350, 700)
 
-# m1 = (256, 289)
-# m2 = (262, 269)
-
-m1 = (271, 236)
+# endpoints of marking on the road
+m1 = (271, 236) # white markings
 m2 = (275, 224)
-#
+# m1 = (262, 268) # space between markings
+# m2 = (271, 235)
+
+# float array of corners in original ROI
 pts1 = np.float32([p00, p10, p11, p01])
+# float array of corners in perspective transform
 pts2 = np.float32([q00, q10, q11, q01])
 
+# size of transformation
 cols_t = max(pts2[:, 0])
 rows_t = max(pts2[:, 1])
 M = cv2.getPerspectiveTransform(pts1, pts2)
@@ -54,7 +76,7 @@ ret, frame1 = cap.read()
 prev = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 
 rows, cols, _ = np.shape(frame1)
-thresh = 1 # 5
+thresh = 3 # 5
 
 pts = np.int32(pts1)
 pts = pts.reshape((-1, 1, 2))
@@ -62,7 +84,7 @@ pts = np.array(pts)
 
 markers = np.float32([[m1, m2]])
 # print np.shape(markers)
-print markers
+# print markers
 
 markers_rect = cv2.perspectiveTransform(markers, M)
 # print markers_rect
@@ -71,14 +93,15 @@ markers = markers[0]
 markers_rect = markers_rect[0]
 marker_len = np.linalg.norm(markers_rect[0] - markers_rect[1])
 # print marker_len
-real_marker_len = 10.0 # ft
+# real_marker_len = 10.0 # ft
+real_marker_len = 5.0 # ft
+
 print "marker length: {}".format(marker_len)
 begin = True
 jj = 0
 
 fps = cap.get(cv2.CAP_PROP_FPS)
 dt = 1./fps # seconds
-dt_h = dt / 3600 # hours
 
 midpt = ptAvg((0,0), (250, 500))
 while(1):
@@ -89,7 +112,6 @@ while(1):
             prev = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
             begin = False
             continue
-        # vel = np.array([])
 
         vels_arr_i = []
         vels_arr_f = []
@@ -126,6 +148,7 @@ while(1):
         vel_i = np.float32([vels_arr_i]) # array
         vel_f = np.float32([vels_arr_f])
         vel_rect_i, vel_rect_f = 0, 0
+
         if vel_i.any():
             # print "any"
             vel_rect_i = cv2.perspectiveTransform(vel_i, M)
@@ -199,8 +222,9 @@ while(1):
         realDisp = avgMag * real_marker_len / marker_len # real-life displacement
         # print realDisp
         realSpeed = realDisp / dt # real-life speed
+        realSpeed_mph = ftps2mph(realSpeed)
         if realSpeed:
-            print realSpeed
+            print realSpeed_mph
         # print realDisp
         cv2.polylines(dispFrame, [pts], True, (0, 255, 255))
         cv2.line(transformed, tuple(markers_rect[0]), tuple(markers_rect[1]), (255, 0, 0), 2)
@@ -209,7 +233,7 @@ while(1):
         cv2.imshow("transformed", transformed)
         if normer:
             # print np.shape(mag_norm)
-            cv2.imshow("vectors", mag_norm)
+            cv2.imshow("normalized magnitudes", mag_norm)
         k = cv2.waitKey(1) & 0xff
         if k == 27:
             break
