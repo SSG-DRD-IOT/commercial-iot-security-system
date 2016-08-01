546f4ff34f821376385c9f739a36eca9bb415845
danbudanov <danbudanov@gmail.com>
2016-08-01 16:51:36 -0700

    commented car speed code; made pretty-er

--- opencv/commercial/Road/car_flow.py
+++ opencv/commercial/Road/car_flow.py
@@ -4,7 +4,7 @@
 # Summer Internship - 2016
 ###############################################################################
 # Title: car_flow.py
-# Project: Car Motion Detection
+# Project: Car Speed Detection
 # Description:
 #   uses optical flow to find car displacement vectors
 #   then, uses a perspective transform to estimate car velocity
@@ -44,203 +44,213 @@ p01 = (423, 210)
 p10 = (82, 355)
 p11 = (544, 356)
 
-# # corners of perspective transform of ROI
-# q00 = (0, 0)
-# q01 = (250, 0)
-# q10 = (0, 500)
-# q11 = (250, 500)
+# size of transformation
+cols_t = 350
+rows_t = 700
 
 # corners of perspective transform of ROI
 q00 = (0, 0)
-q01 = (350, 0)
-q10 = (0, 700)
-q11 = (350, 700)
+q01 = (cols_t, 0)
+q10 = (0, rows_t)
+q11 = (cols_t, rows_t)
 
-# endpoints of marking on the road
-m1 = (271, 236) # white markings
+# endpoints of dash on the road
+m1 = (271, 236)
 m2 = (275, 224)
-# m1 = (262, 268) # space between markings
-# m2 = (271, 235)
 
 # float array of corners in original ROI
 pts1 = np.float32([p00, p10, p11, p01])
 # float array of corners in perspective transform
 pts2 = np.float32([q00, q10, q11, q01])
 
-# size of transformation
-cols_t = max(pts2[:, 0])
-rows_t = max(pts2[:, 1])
+# generate perspective transform matrix from ROI
 M = cv2.getPerspectiveTransform(pts1, pts2)
 
+# retrieve first frame
 ret, frame1 = cap.read()
+rows, cols, _ = np.shape(frame1)
+
+# optical flow operates in grayscale
 prev = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 
-rows, cols, _ = np.shape(frame1)
+# set threshold for velocity
 thresh = 3 # 5
 
+# create ROI corners into array so that its edges can be drawn with polylines
 pts = np.int32(pts1)
 pts = pts.reshape((-1, 1, 2))
 pts = np.array(pts)
 
+# form dash endpoints into an array
 markers = np.float32([[m1, m2]])
-# print np.shape(markers)
-# print markers
-
+# perform perspective transform on dash marker points
 markers_rect = cv2.perspectiveTransform(markers, M)
-# print markers_rect
-
-markers = markers[0]
+# remove extra layer put on markers by transform
 markers_rect = markers_rect[0]
+# find length of markers in rectangular space
 marker_len = np.linalg.norm(markers_rect[0] - markers_rect[1])
-# print marker_len
-# real_marker_len = 10.0 # ft
+# real-world length of markers
 real_marker_len = 5.0 # ft
 
-print "marker length: {}".format(marker_len)
+# beginning step; included because it can be useful for starting real-time view
 begin = True
+# simple counter for debug purposes
 jj = 0
 
+# get video framerate
 fps = cap.get(cv2.CAP_PROP_FPS)
+# calculate time interval between frames; use for velocity calculation
 dt = 1./fps # seconds
 
-midpt = ptAvg((0,0), (250, 500))
+# midpoint of projection frame
+midpt = ptAvg((0,0), (cols_t, rows_t))
+
+# work with every Nth displacement vector
+SHOW_EVERY = 20
+
+# main loop
 while(1):
     ret, frame2 = cap.read()
     jj += 1
     if ret:
         if begin:
+            # if this is the very first frame, calculate prev and continue
+            # use this step for implementing inter-frame timing later on
             prev = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
             begin = False
             continue
+        # initial and final points of displacement vectors
+        disp_arr_i = []
+        disp_arr_f = []
 
-        vels_arr_i = []
-        vels_arr_f = []
-
+        # warp ROI into rectangular space by applying perspective transform
         transformed = cv2.warpPerspective(frame2, M, (cols_t, rows_t))
 
+        # perform color conversion for optical flow
         nxt = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-        if prev is nxt:
-            print "same"
+
+        # set optical flow parameters:
         #             (pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags))
         flowParams = (  0.5,        3,       15,       3,        5,       1.2,      0)
 
+        # calculate optical flow
+        # yields rows x cols x 2 array with x and y components of displacement vector
         flow = cv2.calcOpticalFlowFarneback(prev,nxt, None, *flowParams)
-        # print flow.any()
+        # calculate magnitude of displacement vector
         mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-        # print np.max(mag)
-        # print np.shape(mag)
-        dispFrame = np.copy(frame2)
-        cv2.line(dispFrame, m1, m2, (255, 0, 0), 2)
+
+        # create a copy of original frame on which you draw vectors
+        viewFrame = np.copy(frame2)
+
+        # draw blue line along selected dash on highway
+        cv2.line(viewFrame, m1, m2, (255, 0, 0), 2)
+
+        # find locations where displacement magnitudes surpass threshold
         goodMags = np.where(mag > thresh)
-        # print np.shape(flow)
+
         for i, pos in enumerate(goodMags[0]):
-            # if i % 160 == 0:
-            if i % 20 == 0:
-        # for pos in goodMags[goodMags%160]:
-                y, x = (goodMags[0][i], goodMags[1][i])
+            # to reduce quantity of vecs working with, use every SHOW_EVERY-eth vector
+            if i % SHOW_EVERY == 0:
+                # get position of vector
+                x, y = (goodMags[1][i], goodMags[0][i])
+                # get horizontal and vertical components of vector
                 u, v = flow[y][x]
-                # print "({},{}), ({}, {})".format(x, y, u, v)
-                # print pos
-                cv2.arrowedLine(dispFrame, (x, y), (x+int(round(u)), y+int(round(v))), (0, 0, 255), 1)
-                # np.vstack((vel,  (x, y), (x+int(round(u)), y+int(round(v)))))
-                vels_arr_i.append((x,y))
-                vels_arr_f.append((x+u, y+v))
-        vel_i = np.float32([vels_arr_i]) # array
-        vel_f = np.float32([vels_arr_f])
-        vel_rect_i, vel_rect_f = 0, 0
 
-        if vel_i.any():
-            # print "any"
-            vel_rect_i = cv2.perspectiveTransform(vel_i, M)
-            vel_rect_f = cv2.perspectiveTransform(vel_f, M)
+                # draw the displacement vector
+                cv2.arrowedLine(viewFrame, (x, y), (x+int(round(u)), y+int(round(v))), (0, 0, 255), 1)
 
+                # add displacement vector initial and final points to list
+                disp_arr_i.append((x,y))
+                disp_arr_f.append((x+u, y+v))
+        # make lists into numpy arrays that can be operated on
+        disp_i = np.float32([disp_arr_i])
+        disp_f = np.float32([disp_arr_f])
 
-            vel_rect_i_int = np.int32(vel_rect_i[0])
-            vel_rect_f_int = np.int32(vel_rect_f[0])
+        # set rectangular displacements to 0 by default
+        disp_rect_i, disp_rect_f = 0, 0
 
-            # for i in range(0, np.size(vel_rect_i) - 1):
-            for i, _ in enumerate(vel_rect_i_int):
-                cv2.arrowedLine(transformed, tuple(vel_rect_i_int[i,...]), tuple(vel_rect_f_int[i,...]), (0, 0, 255), 1)
-            # print velMag, "\n"
-            # avgMag = np.average(velMag, axis=1)
-            # print "average: {}\tframe: {}".format(np.shape(avgMag), np.shape(frame2))
-            # print avgMag, '\n'
-        # else:
-        #     print "none shape:\t", vel_i
-        #     # print "not any"
+        # check if displacement vector array has any points in it
+        if disp_i.any():
+            # apply perspective transform on displacement vector start/end points
+            disp_rect_i = cv2.perspectiveTransform(disp_i, M)
+            disp_rect_f = cv2.perspectiveTransform(disp_f, M)
 
-        # print "i:\t{}, f:\t{}".format(np.shape(vel_rect_i), np.shape(vel_rect_f))
-        # print "jj is {}".format(jj)
+            # cast transformed vectors to integers in order to draw them
+            disp_rect_i_int = np.int32(disp_rect_i[0])
+            disp_rect_f_int = np.int32(disp_rect_f[0])
 
-        # print "vi", vel_rect_i
-        # print "vf", vel_rect_f
+            # draw displacement vectors in rectangular space
+            for i, _ in enumerate(disp_rect_i_int):
+                cv2.arrowedLine(transformed, tuple(disp_rect_i_int[i,...]), tuple(disp_rect_f_int[i,...]), (0, 0, 255), 1)
 
-        # print np.shape(vel_rect_i)
-        if type(vel_rect_i) == np.ndarray:
-            # vel_rect_i_x = vel_rect_i[:, 0]
-            # vel_rect_i_y = vel_rect_i[:, 1]
-            # vel_rect_f_x = vel_rect_f[:, 0]
-            # vel_rect_f_y = vel_rect_f[:, 1]
-            vel_rect_i_x = vel_rect_i[:, :, 0]
-            vel_rect_i_y = vel_rect_i[:, :, 1]
-            vel_rect_f_x = vel_rect_f[:, :, 0]
-            vel_rect_f_y = vel_rect_f[:, :, 1]
-            # print np.shape(vel_rect_i_y)
-            # print "xi: {}\nyi: {}\n xf: {}\nyf: {}".format(vel_rect_i_x, \
-            #                                         vel_rect_i_y, \
-            #                                         vel_rect_f_x, \
-            #                                         vel_rect_f_y)
-            velMag, velAng = cv2.cartToPolar(vel_rect_f_x - vel_rect_i_x,\
-                                                vel_rect_f_y - vel_rect_i_y)
-            # print 1
-            # print jj, ":\t", 1
+        if type(disp_rect_i) == np.ndarray:
+            # otherwise, 0 is returned
+            # find x, y components of rectangular displacement vectors
+            disp_rect_i_x = disp_rect_i[:, :, 0]
+            disp_rect_i_y = disp_rect_i[:, :, 1]
+            disp_rect_f_x = disp_rect_f[:, :, 0]
+            disp_rect_f_y = disp_rect_f[:, :, 1]
+            # find magnitudes of displacement vectors in rectangular space
+            dispMag, velAng = cv2.cartToPolar(disp_rect_f_x - disp_rect_i_x,\
+                                                disp_rect_f_y - disp_rect_i_y)
+
         else:
-            # print type(vel_rect_i)
-            # print "int rect i:", vel_rect_i
-            # vel_rect_i_x, vel_rect_i_y, vel_rect_f_x, vel_rect_f_y = 0, 0, 0, 0
-            # velMag, velAng = cv2.cartToPolar(vel_rect_f_x - vel_rect_i_x,\
-                                                # vel_rect_f_y - vel_rect_i_y)
-            # print "xi: {}\nyi: {}\n xf: {}\nyf: {}".format(vel_rect_i_x, \
-                                                    # vel_rect_i_y, \
-                                                    # vel_rect_f_x, \
-                                                    # vel_rect_f_y)
-            velMag = 0
-            # print 0 # pattern: 110110110110110110...
-            # print jj, ":\t", 0
-        # velMag, velAng = cv2.cartToPolar(vel_rect_f_x - vel_rect_i_x,\
-        #                                     vel_rect_f_y - vel_rect_i_y)
-        # print "velmag is", np.shape(velMag)
-        # print velMag, "\n"
+            # otherwise, set displacement magnitude to 0
+            dispMag = 0
+
+        # normalize magnitude map
+        # can later use this for binary mapping
         normer = float(np.max(mag))
-        # print "normer is:", normer
-        if normer:
+        if normer: # checks that normer is not 0
+            # normalize mag
             mag_norm = np.uint8(mag * 255 / normer)
             mag_norm = cv2.applyColorMap(mag_norm, cv2.COLORMAP_JET)
-        # print np.max(velMag)
-        avgMag = np.average(velMag)
+        # find average displacement magnitude
+        avgMag = np.average(dispMag)
 
-        realDisp = avgMag * real_marker_len / marker_len # real-life displacement
-        # print realDisp
+        # using the [known] dash length, convert displacements to real-world distances
+        # nowmalize the displacement relative to dashes and multiply by real-world dash length
+        realDisp = avgMag * real_marker_len / marker_len
+
+        # find real-world speed by dividing displacement by time interval between frames
         realSpeed = realDisp / dt # real-life speed
+        # convert speed into miles per hour
         realSpeed_mph = ftps2mph(realSpeed)
+        # if the real speed isn't 0, print it
         if realSpeed:
             print realSpeed_mph
-        # print realDisp
-        cv2.polylines(dispFrame, [pts], True, (0, 255, 255))
+
+        # draw the ROI boundary in yellow
+        cv2.polylines(viewFrame, [pts], True, (0, 255, 255))
+        # draw line on the dashes in the rectqngular space
         cv2.line(transformed, tuple(markers_rect[0]), tuple(markers_rect[1]), (255, 0, 0), 2)
-        cv2.arrowedLine(transformed, midpt, (midpt[0], midpt[1]+int(avgMag)), (0, 0, 0), 2)
-        cv2.imshow("road", dispFrame)
+
+        # draw black line down middle of rectangular space showing length of average vector
+        # cv2.arrowedLine(transformed, midpt, (midpt[0], midpt[1]+int(avgMag)), (0, 0, 0), 2)
+
+        # show road with marked ROI
+        cv2.imshow("road", viewFrame)
+        # show rectanguler space
         cv2.imshow("transformed", transformed)
+
         if normer:
-            # print np.shape(mag_norm)
+            # show region with normalized magnitudes
             cv2.imshow("normalized magnitudes", mag_norm)
+
+        # if user presses ESC then exit
         k = cv2.waitKey(1) & 0xff
         if k == 27:
             break
 
+        # previously new gray frame becomes old one
         prev = nxt
+
     else:
+        # if no more frames are returned from video than end video capture
         break
+
 print "done"
+
+# release video capture and destroy windows
 cap.release()
 cv2.destroyAllWindows
